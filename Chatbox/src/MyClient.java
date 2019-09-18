217b67afce9fc0a47c0194ae7e7dfb661c5d2a46
Wajiddat18c <waji0003@stud.kea.dk>
2019-09-18 23:38:04 +0200

    Made oneway connection

--- Chatbox/src/MyClient.java
+++ Chatbox/src/MyClient.java
@@ -6,21 +6,19 @@ class MyClient {
     public static void main(String args[]) throws Exception {
         Thread beat;
         Socket s = new Socket("localhost", 4567);
-        DataInputStream din = new DataInputStream(s.getInputStream());
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 //        final long startTime = System.nanoTime();
 //        ClientBeat clientBeat = new ClientBeat();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = "", str2 = "";
+        String str = "";
         while (!str.equals("stop")) {
 
 //                clientBeat.run();
                 str = br.readLine();
                 dout.writeUTF(str);
                 dout.flush();
-                str2 = din.readUTF();
-                System.out.println("Server says: " + str2);
+                System.out.println("Modtaget");
             }
 
         dout.close();
--- Chatbox/src/MyServer.java
+++ Chatbox/src/MyServer.java
@@ -11,29 +11,25 @@ class MyServer {
         Socket s = ss.accept();
         DataInputStream din = new DataInputStream(s.getInputStream());
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        FileWriter writer = new FileWriter("C:\\Users\\wajid\\Desktop\\Github reops\\Chatbox\\Chatbox\\Chatbox\\src\\Log\\chatlog.txt", true);
-        BufferedWriter buffer = new BufferedWriter(writer);
         Logger logger = Logger.getLogger("MyLog");
         FileHandler fileHandler = new FileHandler("C:\\Users\\wajid\\Desktop\\Github reops\\Chatbox\\Chatbox\\Chatbox\\src\\Log\\chatlog.txt", true);
         SimpleFormatter formatter = new SimpleFormatter();
         fileHandler.setFormatter(formatter);
         logger.addHandler(fileHandler);
 
+
         String str = "", str2 = "";
         while (!str.equals("stop")) {
 
             str = din.readUTF();
             System.out.println("client says: " + str);
-            str2 = br.readLine();
+
             dout.writeUTF(str2);
             logger.info(str);
-//            buffer.append(str);
             dout.flush();
         }
 
 
-        buffer.close();
         din.close();
         s.close();
         ss.close();
