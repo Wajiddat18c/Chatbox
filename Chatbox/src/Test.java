ab38822206023935efa69ea52e0288b81b56cf6c
Wajiddat18c <waji0003@stud.kea.dk>
2019-09-19 02:30:28 +0200

    .

--- Chatbox/src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        final long startTime = System.nanoTime();
-        final long duration = System.nanoTime() - startTime;
-        System.out.println(duration);
-
-    }
-}
