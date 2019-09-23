ee0a032e21c3a0e62fad5034f3c94d28ea782e2b
Wajiddat18c <waji0003@stud.kea.dk>
2019-09-23 09:24:26 +0200

    .

--- Chatbox/src/ServerThread.java
+++ Chatbox/src/ServerThread.java
@@ -39,7 +39,7 @@ public class ServerThread implements Runnable {
         DataInputStream din = new DataInputStream(s.getInputStream());
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
         Logger logger = Logger.getLogger("MyLog");
-        FileHandler fileHandler = new FileHandler("E:\\Users\\Ejer\\Documents\\GitHub\\Chatbox\\Chatbox\\src\\Log\\chatlog.txt", true);
+        FileHandler fileHandler = new FileHandler("./Log/chatlog.txt", true);
         SimpleFormatter formatter = new SimpleFormatter();
         fileHandler.setFormatter(formatter);
         logger.addHandler(fileHandler);
